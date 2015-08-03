a0d159f2ea2bdec80c23da86a9ebc565ad6b34bd
logan dougher <logan.dougher@gmail.com>
2015-08-03 15:10:46 -0500

    moved routes into separate files. alternative title added

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+// npm requires
 var express = require('express');
+
+//
+var routes = require('./routes/index');
+var pizza = require('./routes/pizza');
+
+// variables
 var app = express();
 
+// settings
 app.set('view engine', 'ejs');
+app.set('case sensitive routing', true);
 
-// call 'next' for middlewares that are part of the chain
+app.locals.title = 'Kewl';
+
+// middlewares
 /*app.use(function (req, res, next) {
   console.log('Request at ' + new Date().toISOSting());
   next();
@@ -11,56 +22,11 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
-//////route, callback
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// routes
+app.use('/', routes);
+app.use('/pizza', pizza);
 
-app.get('/pizza/:topping/:qty', function (req, res) {
-  var obj = req.params;
-  obj.title = 'Pizza Shop';
-  res.render('templates/pizza', req.params);
-});
-
-app.get('/hello', function (req, res) {
-  res.send('Hello!');
-});
-
-app.get('/awesomethings', function (req, res) {
-  setTimeout(function () {
-
-    var awesomeThings = [
-      'pizza',
-      'randy savage',
-      'buddy',
-    ];
-
-    res.render('templates/world',
-      { title: 'awesomesite.com',
-        welcome: 'thanks for coming!',
-        awesomeThings: awesomeThings
-      }
-    );
-  }), 5000;
-});
-
-app.get('/test', function (req, res, next) {
-  res.write('test1');
-  next();
-});
-
-app.get('/test', function (req, res) {
-  res.end('test2');
-});
-
-app.get('/json', function (req, res) {
-  res.send({an: 'object'});
-});
-
-app.get('/thisshoulderror', function (req, res) {
-  res.send(badVariable);
-});
-
+// errors
 app.use(function (req, res) {
   res.status(403).send('Unauthorized!');
 });
@@ -86,3 +52,5 @@ var server = app.listen(3000, function () {
 // want logging functions early in the chain
 
 // Errors should be 400's before 500's
+
+// locals are like global constants that are available everywhere.
---
+++ routes/index.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+
+/// ROUTES ////
+
+//////route, callback
+router.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+router.get('/hello', function (req, res) {
+  res.send('Hello!');
+});
+
+router.get('/awesomethings', function (req, res) {
+  setTimeout(function () {
+
+    var awesomeThings = [
+      'pizza',
+      'randy savage',
+      'buddy',
+    ];
+
+    res.render('templates/world',
+      { welcome: 'thanks for coming!',
+        awesomeThings: awesomeThings
+      }
+    );
+  }); 5000;
+});
+
+router.get('/test', function (req, res, next) {
+  res.write('test1');
+  next();
+});
+
+router.get('/test', function (req, res) {
+  res.end('test2');
+});
+
+router.get('/json', function (req, res) {
+  res.send({an: 'object'});
+});
+
+router.get('/thisshoulderror', function (req, res) {
+  res.send(badVariable);
+});
+
+module.exports = router;

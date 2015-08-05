b30cfddffc223111799b434b549059434c38c4e4
logan dougher <logan.dougher@gmail.com>
2015-08-04 20:33:11 -0500

    added loggly.js and installed loggly and morgan

--- app.js
+++ app.js
@@ -1,58 +1,73 @@
-// npm requires
+var fs = require('fs');
+
 var express = require('express');
 var lessCSS = require('less-middleware');
-//
+var morgan = require('morgan');
+var bodyParser = require('body-parser')
+
 var routes = require('./routes/index');
 var pizza = require('./routes/pizza');
+var chickennuggets = require('./routes/chickennuggets');
 
-// variables
 var app = express();
 
-// settings
 app.set('view engine', 'ejs');
 app.set('case sensitive routing', true);
 
-app.locals.title = 'Kewl';
+app.locals.title = 'aweso.me';
 
-// middlewares
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(lessCSS('public'));
 
-/*app.use(function (req, res, next) {
-  console.log('Request at ' + new Date().toISOSting());
+var logStream = fs.createWriteStream('access.log', {flags: 'a'});
+app.use(morgan('combined', {stream: logStream}));
+app.use(morgan('dev'));
+
+app.use(function (req, res, next) {
+  var client = require('./lib/loggly')('incoming');
+
+  client.log({
+    ip: req.ip,
+    date: new Date(),
+    url: req.url,
+    status: res.statusCode,
+    method: req.method
+  });
   next();
-});*/
+});
 
 app.use(express.static('public'));
 
-// routes
+
+
 app.use('/', routes);
 app.use('/pizza', pizza);
+app.use('/chickennuggets', chickennuggets);
 
-// errors
 app.use(function (req, res) {
   res.status(403).send('Unauthorized!');
 });
 
 app.use(function (err, req, res, next) {
-  // pass 4 arguments to create error handling middleware
-  console.log('ERRRRRR!', err.stack);
+  var client = require('./lib/loggly')('error');
+
+  client.log({
+    ip: req.ip,
+    date: new Date(),
+    url: req.url,
+    status: res.statusCode,
+    method: req.method,
+    stackTrace: err.stack
+  });
+
+  // pass 4 arguments to create an error handling middleware
+  console.log('ERRRRRRRRRR', err.stack);
   res.status(500).send('My Bad');
 });
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
-// %s:%s takes a string value in order. host and port replace the %s's
-  console.log('Example app listening at http://%s:%s', host, port);
+
+  console.log('Example app listening at http://%s:%d', host, port);
 });
-
-// express allows chaining .get startments for routing,
-// instead of several if else statements.
-
-// at its core, express is a routing engine.
-
-// want logging functions early in the chain
-
-// Errors should be 400's before 500's
-
-// locals are like global constants that are available everywhere.
